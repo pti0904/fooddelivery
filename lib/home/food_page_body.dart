169a6d93bc028bb596cd6a55778908a2758307fb
Park <pti0904@naver.com>
2023-01-09 12:32:14 +0900

    1:50

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fooddelivery/utils/colors.dart';
+import 'package:fooddelivery/widgets/big_text.dart';
+import 'package:fooddelivery/widgets/icon_and_text_widget.dart';
+import 'package:fooddelivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -9,11 +13,30 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue=0.0;
+  double _scaleFactor=0.8;
+  double _height = 220;
+  @override
+  void initState(){
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+        //print("Current value is" +_currPageValue.toString());
+//Widget _buildPageItem에서 계속
+      });
+    });
+  }
+  @override
+  void dispose(){
+    pageController.dispose();
+  }
+
   //첫번째 스택에서 왼쪽,오른쪽 내용 나오게하기위해
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.redAccent,
+
       height: 320,
       child: PageView.builder(
         controller: pageController,
@@ -26,44 +49,100 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Stack(
-      //continue take the size or height
-      //container안에 넣어도 사이즈 유지하기위해
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 5, right: 5),
-          decoration: BoxDecoration(
-              color: index.isEven ? Color(0xff69c5df) : Color(0xFF9294cc),
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/image/food1.jpg")),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xFF69c5df),
-                  offset: Offset(0, 3),
-                  blurRadius: 6,
-                )
-              ]),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-              height: 140,
-              margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-            child: Container(
-              padding: EdgeInsets.only(top: 10, left: 15, right: 10),
-              
+    Matrix4 matrix = new Matrix4.identity();
+    if(index==_currPageValue.floor()){
+      var currScale= 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
 
+    }else if(index ==_currPageValue.floor()+1){
+      var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }//밑에 Stack을 Transform 으로 감싸줌
+    else if(index ==_currPageValue.floor()-1){
+      var currScale= 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        //continue take the size or height
+        //container안에 넣어도 사이즈 유지하기위해
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
+            decoration: BoxDecoration(
+                color: index.isEven ? Color(0xff69c5df) : Color(0xFF9294cc),
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/image/food1.jpg")),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFF69c5df),
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                  )
+                ]),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                height: 120,
+                margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              child: Container(
+                padding: EdgeInsets.only(top: 15, left: 15, right: 10),
+                child: Column(
+                  crossAxisAlignment:  CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Chinese Side"),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor,size : 15,))
+                        ),//똑같은걸로 채우기위해,
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "4.5"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "1287"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "comments")
+                      ],
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      children: [
+                        IconAndTextWidget(icon: Icons.circle_sharp, text: "Normal",
+                             iconColor: AppColors.iconColor1),
+                        IconAndTextWidget(icon: Icons.location_on, text: "1.7km",
+                            iconColor: AppColors.mainColor),
+                        IconAndTextWidget(icon: Icons.access_time_rounded, text: "32min",
+                            iconColor: AppColors.iconColor2)
+                      ],
+
+                    )
+                  ],
+                )
+
+
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,7 +4,7 @@ class AppColors{
   static const textColor = Color(0xFFccc7c5);
   static const mainColor = Color(0xFF89dad0);
   static const iconColor1 = Color(0xFFffd28d);
-  static const iconColor2 = Color(0xFF8f837f);
+  static const iconColor2 = Color(0xFFfcab88);
   static const paraColor = Color(0xFF8f837f);
   static const buttonBackgroundColor = Color(0xFFf7f6f4);
   static const signColor = Color(0xFFa9a29f);
